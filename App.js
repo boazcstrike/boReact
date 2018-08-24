2d30234161c43dc05b1a1b40546ad22a26b24704
Boaz Sze <boazcstrike@yahoo.com.sg>
2018-08-24 23:16:34 +0800

    Initial setups for the firebase and redux

--- App.js
+++ App.js
@@ -6,44 +6,95 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+// import React, {Component} from 'react';
+// import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
-type Props = {};
-export default class App extends Component<Props> {
+// // type Props = {};
+// // export default class App extends Component<Props> {
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>Welcome to React Native!</Text>
+//         <Text style={styles.instructions}>To get started, edit App.js</Text>
+//         <Text style={styles.instructions}>{instructions}</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { Font, AppLoading } from 'expo';
+
+import Router from './app/config/routes'
+import store from './app/redux/store';
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false,
+    }
+  }
+
+  async _loadAssetsAsync() {
+    const fontAssets = cacheFonts([
+      { RobotoExtraBold: require('./app/assets/fonts/Roboto-Black.ttf') },
+      { RobotoBold: require('./app/assets/fonts/Roboto-Bold.ttf') },
+      { RobotoMedium: require('./app/assets/fonts/Roboto-Medium.ttf') },
+      { RobotoRegular: require('./app/assets/fonts/Roboto-Regular.ttf') },
+      { RobotoLight: require('./app/assets/fonts/Roboto-Light.ttf') }
+    ]);
+
+    await Promise.all([...fontAssets]);
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider store={store}>
+        <Router />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyDic4uMaI6iA2-IKXwUwP4QYrlNqsZHlXw";
+export const FIREBASE_AUTH_DOMAIN = "boreact-e5f97.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://boreact-e5f97.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "boreact-e5f97";
+export const FIREBASE_STORAGE_BUCKET = "boreact-e5f97.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "220150371329";
+                    
+export const FACEBOOK_APP_ID = "1921339831500321";
