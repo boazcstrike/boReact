386e8be3c4d4ec666665b2207ffd0cb252e51a46
Boaz Sze <boazcstrike@yahoo.com.sg>
2018-08-28 18:11:21 +0800

    Modals not working

--- App.js
+++ App.js
@@ -1,116 +1,125 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-// import React, {Component} from 'react';
-// import {Platform, StyleSheet, Text, View} from 'react-native';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
-
-// // type Props = {};
-// // export default class App extends Component<Props> {
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>Welcome to React Native!</Text>
-//         <Text style={styles.instructions}>To get started, edit App.js</Text>
-//         <Text style={styles.instructions}>{instructions}</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
+import { Modal, TouchableHighlight, Text, View } from 'react-native';
+import { name as appName } from './app.json';
 
-import store from './app/store'; //Import the store
-import Home from './app/components/home' //Import the component file
+export default class HelloWorldApp extends Component {
+    state = {
+        modalVisibility: false,
+        modalId: 0,
+    };
 
-export default class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    );
-  }
+    setModalVisible(visibility){
+        this.setState({
+            modalVisibility: visibility
+        });
+    }
+
+    SwitchModalContent(modalId, visibility){
+        this.setState({
+            modalVisibility: visibility
+        });
+        switch(modalId){
+            default: {
+                return (
+                    <Modal
+                        animationType="slide"
+                        transparent={false}
+                        visible={this.state.modalVisibility}
+                        onRequestClose={() => {
+                            alert('Modal has been closed.');
+                        }}>
+                        <View style={{ marginTop: 22 }}>
+                            <View>
+                                <Text>default to {appName} inside a modal!</Text>
+
+                                <TouchableHighlight
+                                    onPress={() => {
+                                        this.setModalVisible(!this.state.modalVisibility);
+                                    }}>
+                                    <Text>Hide Modal</Text>
+                                </TouchableHighlight>
+                            </View>
+                        </View>
+                    </Modal>
+                )
+            }
+            case 0: {
+                return (
+                    <Modal
+                        animationType="slide"
+                        transparent={false}
+                        visible={this.state.modalVisibility}
+                        onRequestClose={() => {
+                            alert('Modal has been closed.');
+                        }}>
+                        <View style={{ marginTop: 22 }}>
+                            <View>
+                                <Text>1 to {appName} inside a modal!</Text>
+
+                                <TouchableHighlight
+                                    onPress={() => {
+                                        this.setModalVisible(!this.state.modalVisibility);
+                                    }}>
+                                    <Text>Hide Modal</Text>
+                                </TouchableHighlight>
+                            </View>
+                        </View>
+                    </Modal>
+                )
+            }
+            case 1: {
+                return (
+                    <Modal
+                        animationType="slide"
+                        transparent={false}
+                        visible={this.state.modalVisibility}
+                        onRequestClose={() => {
+                            alert('Modal has been closed.');
+                        }}>
+                        <View style={{ marginTop: 22 }}>
+                            <View>
+                                <Text>2 to {appName} inside a modal!</Text>
+
+                                <TouchableHighlight
+                                    onPress={() => {
+                                        this.setModalVisible(!this.state.modalVisibility);
+                                    }}>
+                                    <Text>Hide Modal</Text>
+                                </TouchableHighlight>
+                            </View>
+                        </View>
+                    </Modal>
+                )
+            }
+        }
+    }
+
+    render() {
+        return (
+            <View style={{ marginTop: 22 }}>
+                <Text>Welcome to {appName}</Text>
+
+                <TouchableHighlight
+                    onPress={() => {
+                        this.SwitchModalContent('default', true);
+                    }}>
+                    <Text>Show default Modal</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight
+                    onPress={() => {
+                        this.SwitchModalContent(0, true);
+                    }}>
+                    <Text>Show first Modal</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight
+                    onPress={() => {
+                        this.SwitchModalContent(1, true);
+                    }}>
+                    <Text>Show Second Modal</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
 }
-
-// import React, { Component } from 'react';
-// import { Provider } from 'react-redux';
-// import { Font, AppLoading } from 'expo';
-
-// import Router from './app/config/routes'
-// import store from './app/redux/store';
-
-// function cacheFonts(fonts) {
-//   return fonts.map(font => Font.loadAsync(font));
-// }
-
-// export default class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       isReady: false,
-//     }
-//   }
-
-//   async _loadAssetsAsync() {
-//     const fontAssets = cacheFonts([
-//       { RobotoExtraBold: require('./app/assets/fonts/Roboto-Black.ttf') },
-//       { RobotoBold: require('./app/assets/fonts/Roboto-Bold.ttf') },
-//       { RobotoMedium: require('./app/assets/fonts/Roboto-Medium.ttf') },
-//       { RobotoRegular: require('./app/assets/fonts/Roboto-Regular.ttf') },
-//       { RobotoLight: require('./app/assets/fonts/Roboto-Light.ttf') }
-//     ]);
-
-//     await Promise.all([...fontAssets]);
-//   }
-
-//   render() {
-//     if (!this.state.isReady) {
-//       return (
-//         <AppLoading
-//           startAsync={this._loadAssetsAsync}
-//           onFinish={() => this.setState({ isReady: true })}
-//           onError={console.warn}
-//         />
-//       );
-//     }
-
-//     return (
-//       <Provider store={store}>
-//         <Router />
-//       </Provider>
-//     );
-//   }
-// }
